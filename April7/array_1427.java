0943001f3b214ce8396325164ecf4f245b46cda7
Ilpyo Yang <67682215+Ilpyo-Yang@users.noreply.github.com>
2021-04-07 21:37:26 +0900

    baekjoon

---
+++ April7/array_1427.java
@@ -0,0 +1,25 @@
+package April7;
+
+import java.util.*;
+
+public class array_1427 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		char[] arr = str.toCharArray();
+		char[] reverse = new char[arr.length];
+		
+		Arrays.sort(arr);
+
+		for (int i=0; i<arr.length; i++) {
+			reverse[arr.length-1-i] = arr[i];
+		}
+		
+		String reversedStr = new String(reverse); 
+		System.out.println(reversedStr);
+
+		sc.close();
+	}
+
+}
